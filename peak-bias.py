2c8bf7aa1c9b9aadfb0b71af57b248aee05e0418
liuxx479 <liuxx479@gmail.com>
2017-03-13 13:59:58 -0400

    add Cvir

--- peak-bias.py
+++ peak-bias.py
@@ -8,23 +8,65 @@ import scipy.optimize as op
 import sys, os
 import hmf
 
-z0 = 0.4
-ngal_mean = 20 # arcmin^-2
-zlo,zhi=0, 2.0
+###### constants ######
+c = 299792.458#km/s
+Gnewton = 6.674e-8#cgs cm^3/g/s^2
+H0 = 70.0#67.74
+h = H0/100.0
+OmegaM = 0.30#1#Planck15 TT,TE,EE+lowP+lensing+ext
+OmegaV = 1.0-OmegaM
+#rho_c0 = 9.9e-30#g/cm^3
+M_sun = 1.989e33#gram
+# growth factor
+Hcgs = lambda z: H0*sqrt(OmegaM*(1+z)**3+OmegaV)*3.24e-20
+H_inv = lambda z: 1.0/(H0*sqrt(OmegaM*(1+z)**3+OmegaV))
+# luminosity distance Mpc
+DC_integral = lambda z: c*quad(H_inv, 0, z)[0]
+z_arr = linspace(0.1, 1.4, 1000)
+DC_arr0 = array([DC_integral(z) for z in z_arr])
+DC = interpolate.interp1d(z_arr, DC_arr0)
+DA = lambda z: DC(z)/(1.0+z)
+DL = lambda z: DC(z)*(1.0+z)
+##rho_cz = lambda z: rho_c0*(OmegaM*(1+z)**3+(1-OmegaM))
+rho_cz = lambda z: 0.375*Hcgs(z)**2/pi/Gnewton#critical density
 
-Pz = lambda z: 0.5*z**2/z0**3*exp(-z/z0)
-z_choices = linspace(zlo,zhi,101)
+
+
+###### source galaxies ######
+z0 = 0.4 ## mean redshift 
+ngal_mean = 20 ## number density in unit of arcmin^-2
+zlo, zhi= 0, 2.0 ## the redshift cuts
+Pz = lambda z: 0.5*z**2/z0**3*exp(-z/z0) ## P(z)
+z_choices = linspace(zlo,zhi,101) ## center of z bins
 prob = Pz(z_choices)
 prob /= sum(prob)
+## redshift and Ngal generator
+redshift_gen = lambda N: np.random.choice(z_choices, size=N, p=prob)
 Ngal_gen = lambda N: np.random.poisson(N)
 
-redshift_gen = lambda N: np.random.choice(z_choices, size=N, p=prob)
 
-### halo mass function gen 
-#dndm_arr = array([hmf.MassFunction(z=iz, Mmin=10, Mmax=15, dlog10m=0.05).dndm for iz in z_choices])
-#save('dndm_arr.npy',dndm_arr)
-z_arr, M_arr = z_choices, arange(10,15,0.05)
-dndm_arr = load('dndm_arr.npy')
-#dndm = interpolate.RectBivariateSpline(z_arr, M_arr, dndm_arr)
+
+###### halo mass function ######
+Mmin, Mmax, dlog10m=12, 15, 0.05
+dndm_arr = array([hmf.MassFunction(z=iz, Mmin=Mmin, Mmax=Mmax, dlog10m=dlog10m).dndm for iz in z_choices])
+# save('dndm_arr.npy',dndm_arr)
+z_arr, M_arr = z_choices, arange(Mmin,Mmax,dlog10m)
+#dndm_arr = load('dndm_arr.npy')
+dndm_arr /= sum(dndm_arr,axis=1).reshape(-1,1)
+#### dndm = interpolate.RectBivariateSpline(z_arr, M_arr, dndm_arr)
+
+
+
+###### lens galaxies ######
+z_lens = redshift_gen(1) ## lens redshift
+M_lens = np.random.choice(M_arr, p=dndm_arr[z_arr==z_lens][0]) ## find lens mass
+### HOD
+A, B, C = 47.0, 0.85, -0.1
+N_lens_fcn = lambda logM, z: A*(10**(logM-14.0)/h)**B*(1+z)**C - 1.0
+N_lens = int(N_lens_fcn(M_lens, z_lens) + 0.5)
+print M_lens, z_lens, N_lens
+Cvir = lambda logM, z: 10**(0.537+0.488*exp(-0.718*z**1.08)+(-0.097+0.024*z)* (logM-12-log10(2.0/h))  )
+
+#rho_s = M_vir/(4\pi [r_s]^3) [ln(1+c_vir)-c_vir/(1+c_vir)]^{^1}
 
 
