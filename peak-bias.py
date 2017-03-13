0fa3e068ea900c390d5b2780daf7c0a876fb00cb
liuxx479 <liuxx479@gmail.com>
2017-03-13 12:11:04 -0400

    add hmf

--- peak-bias.py
+++ peak-bias.py
@@ -6,29 +6,25 @@ from scipy import interpolate,stats
 from scipy.integrate import quad
 import scipy.optimize as op
 import sys, os
+import hmf
 
 z0 = 0.4
 ngal_mean = 20 # arcmin^-2
+zlo,zhi=0, 2.0
+
 Pz = lambda z: 0.5*z**2/z0**3*exp(-z/z0)
+z_choices = linspace(zlo,zhi,101)
+prob = Pz(z_choices)
+prob /= sum(prob)
+Ngal_gen = lambda N: np.random.poisson(N)
 
-#z_arr = linspace(0,3,100)
-#plot(z_arr,Pz(z_arr)); xlabel('z');ylabel('P(z)')
-#savefig('Pz.png');close()
+redshift_gen = lambda N: np.random.choice(z_choices, size=N, p=prob)
 
-def redshift_gen (N, zlo=0, zhi=2, Pz=Pz):
-    '''Generate the N redshifts with with distribution Pz
-    '''
-    z_choices = linspace(zlo,zhi,2001)
-    prob = Pz(z_choices)
-    sample = np.random.choice(z_choices, size=N, p=prob/sum(prob))
-    return sample
-
-#### test
-#sample=redshift_gen(100)
-#hist(sample);xlabel('z');ylabel('hist');savefig('test_reshiftgen.png');close()
-
-
-
-
+### halo mass function gen 
+#dndm_arr = array([hmf.MassFunction(z=iz, Mmin=10, Mmax=15, dlog10m=0.05).dndm for iz in z_choices])
+#save('dndm_arr.npy',dndm_arr)
+z_arr, M_arr = z_choices, arange(10,15,0.05)
+dndm_arr = load('dndm_arr.npy')
+#dndm = interpolate.RectBivariateSpline(z_arr, M_arr, dndm_arr)
 
 
