884609b0bf901b616a65495b82459f7de6ec918e
liuxx479 <liuxx479@gmail.com>
2017-03-13 18:51:23 -0400

    add fig 4-6 to notebook

--- peak-bias.py
+++ peak-bias.py
@@ -6,8 +6,9 @@ from scipy import interpolate,stats
 from scipy.integrate import quad
 import scipy.optimize as op
 import sys, os
-import hmf
+import hmf ## from https://github.com/steven-murray/hmf
 
+test_run = 1
 ###### constants ######
 Mpc=3.086e+24#cm
 c = 299792.458#km/s
@@ -49,7 +50,6 @@ redshift_gen = lambda N: np.random.choice(z_choices, size=N, p=prob)
 Ngal_gen = lambda N: np.random.poisson(N)
 
 
-
 ###### halo mass function ######
 Mmin, Mmax, dlog10m=12, 15, 0.05
 dndm_arr = array([hmf.MassFunction(z=iz, Mmin=Mmin, Mmax=Mmax, dlog10m=dlog10m).dndm for iz in z_choices])
@@ -85,40 +85,53 @@ def Gx_fcn (x, cNFW):#=5.0):
         out = 0
     return out
 
-def kappa_proj (logM,  z_lens, z_source, x_source, y_source, x_lens=0, y_lens=0):
+def kappa_proj (logM,  z_lens, z_source_arr, x_source_arr, y_source_arr, x_lens=0, y_lens=0):
     '''return a function, for certain foreground halo, 
     calculate the projected mass between a foreground halo and a background galaxy pair. 
-    x, y are in radians.
+    x, y are in arcmin.
     ''' 
-    if z_lens>=z_source or Mvir==0:
-        return 0.0
-    else:
-        Mvir=10**logM
-        Rvir=Rvir_fcn(Mvir,z_lens)
-        DC_fore, DC_back = DC(z_lens), DC(z_source)
-        cNFW = Cvir(logM, z)
-        f=1.0/(log(1.0+cNFW)-cNFW/(1.0+cNFW))# = 1.043 with cNFW=5.0
-        two_rhos_rs = Mvir*M_sun*f*cNFW**2/(2*pi*Rvir**2)#cgs, see LK2014 footnote
-        
-        Dl_cm = 3.08567758e24*DC_fore/(1.0+z_lens)
-        ## note: 3.08567758e24cm = 1Mpc###    
-        SIGMAc = 347.29163*DC_back*(1+z_lens)/(DC_fore*(DC_back-DC_fore))
-        ## note: SIGMAc = 1.07163e+27/DlDlsDs
-        ## (c*1e5)**2/4.0/pi/Gnewton = 1.0716311756473212e+27
-        ## 347.2916311625792 = 1.07163e+27/3.08567758e24
-        theta = sqrt((x_lens-x_source)**2+(y_lens-y_source)**2)
-        x = cNFW*theta*Dl_cm/Rvir 
-        ## note: x=theta/theta_s, theta_s = theta_vir/c_NFW
-        ## theta_vir=Rvir/Dl_cm
-        Gx = Gx_fcn(x, cNFW)
-        kappa_p = two_rhos_rs/SIGMAc*Gx        
-        return kappa_p
 
-Rvir=Rvir_fcn(Mvir,z_lens)
+    Mvir = 10**logM
+    Rvir = Rvir_fcn(Mvir,z_lens)
+    DC_lens, DC_source = DC(z_lens), DC(z_source_arr)
+    cNFW = Cvir(logM, z)
+    f = 1.0/(log(1.0+cNFW)-cNFW/(1.0+cNFW))# = 1.043 with cNFW=5.0
+    two_rhos_rs = Mvir*M_sun*f*cNFW**2/(2*pi*Rvir**2)#cgs, see LK2014 footnote
+    
+    Dl_cm = 3.08567758e24*DC_lens/(1.0+z_lens)
+    ## note: 3.08567758e24cm = 1Mpc###    
+    SIGMAc = 347.29163*DC_source*(1+z_lens)/(DC_lens*(DC_source-DC_lens))
+    ## note: SIGMAc = 1.07163e+27/DlDlsDs
+    ## (c*1e5)**2/4.0/pi/Gnewton = 1.0716311756473212e+27
+    ## 347.2916311625792 = 1.07163e+27/3.08567758e24
+    theta_arcmin = sqrt((x_lens-x_source_arr)**2+(y_lens-y_source_arr)**2)
+    theta = radians(theta_arcmin/60.0)
+    x = cNFW*theta*Dl_cm/Rvir 
+    ## note: x=theta/theta_s, theta_s = theta_vir/c_NFW
+    ## theta_vir=Rvir/Dl_cm
+    Gx_arr = array([Gx_fcn(ix, cNFW) for ix in x])
+    kappa_p = two_rhos_rs/SIGMAc*Gx  
+    kappa_p[z_source_arr<z_lens]=0
+    source_contribution = exp(-0.5*theta**2/radians(1.0/60.0)**2)
+    kappa = sum(kappa_p * source_contribution)/sum(source_contribution)
+    return kappa
+
+Rvir = Rvir_fcn(Mvir,z_lens)
 ngal_like_fcn = lambda cNFW: array([Gx_fcn(ix, cNFW) for ix in linspace(0.01, cNFW, 51)])
 ngal_like = ngal_like_fcn(cNFW)/sum(ngal_like_fcn(cNFW))
 rlenses = Rvir/Mpc/DC(z_lens) * np.random.choice(linspace(0.01, 1.0, 51), size=N_lens, p=ngal_like)#radians
 ang_lenses = rand(N_lens)*2*pi
-xlens = degrees(rlenses)*60. * sin(ang_lenses) ## in arcmin
-ylens = degrees(rlenses)*60. * cos(ang_lenses) ## in arcmin
+xlens = degrees(rlenses)*60.0 * sin(ang_lenses) ## in arcmin
+ylens = degrees(rlenses)*60.0 * cos(ang_lenses) ## in arcmin
 
+###### one test run #######
+if test_run:
+#### test on a 10x10 arcmin^2 area
+area = 100.0 ## arcmin^2
+N_gal = Ngal_gen(ngal_mean * area)
+z_source_arr = redshift_gen(N_gal)
+x_source_arr = rand(N_gal) * 10.0
+y_source_arr = rand(N_gal) * 10.0
+    
+    
+    
